e2e9fc4e9ca959b2d8f1a1e1c4b8cb9b0e3b6433
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2018-06-19 12:49:54 +0100

    Mask sensitive output & make it easier to tweak which fields are included

--- src/main/scala/uk/gov/hmrc/kibana/KibanaQuery.scala
+++ src/main/scala/uk/gov/hmrc/kibana/KibanaQuery.scala
@@ -20,7 +20,7 @@ import java.io.FileInputStream
 
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
-import play.api.libs.json.{JsObject, JsValue, Json}
+import play.api.libs.json.{JsObject, JsString, JsValue, Json}
 import play.api.libs.ws.ahc.AhcWSClient
 import play.api.libs.ws.{WSAuthScheme, WSClient}
 
@@ -28,6 +28,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 import scala.language.postfixOps
+import scala.util.matching.Regex
 
 object KibanaQuery {
 
@@ -52,6 +53,9 @@ object KibanaQuery {
       .andThen { case _ => system.terminate() }
   }
 
+//  private final val Fields = Seq("@timestamp", "request", "http_referrer", "sent_http_location")
+  private final val Fields = Seq("@timestamp", "request", "status", "upstream_status", "request_time", "upstream_http_server", "upstream_response_length")
+
   private def runQuery(ws: WSClient, kibanaUrl: String, username: String, password: String, queryJsonFilename: String): Future[Unit] = {
     val query = parseJsonFile(queryJsonFilename)
     ws.url(searchUrl(kibanaUrl))
@@ -67,14 +71,33 @@ object KibanaQuery {
       val hitSources = (response.json \ "hits" \ "hits").as[Seq[JsObject]].map(hit => (hit \ "_source").as[JsObject])
 
       val lines = hitSources.map { source =>
-        def f(name: String) = (source \ name).as[String]
+        def f(name: String): String = {
+          val jsValue = (source \ name).get
+          jsValue match {
+            // don't surround strings with ""
+            case s: JsString => s.value
+            case v => v.toString
+          }
+        }
 
-        s"${f("@timestamp")}\t${f("request")}\t${f("http_referrer")}\t${f("sent_http_location")}"
+        val values = Fields.map(f _ andThen maskNino andThen maskRenewalBarcode).mkString("\t")
+        values
       }
 
-      println("$@timestamp\trequest\thttp_referrer\tsent_http_location")
+      val headings = Fields.mkString("\t")
+      println(headings)
       lines.foreach(println)
     }
+  }
+
+  private val ninoRegex: Regex = "((?!(BG|GB|KN|NK|NT|TN|ZZ)|(D|F|I|Q|U|V)[A-Z]|[A-Z](D|F|I|O|Q|U|V))[A-Z]{2})[0-9]{6}[A-D]?".r.unanchored
+  private def maskNino(s: String): String = {
+    ninoRegex.replaceAllIn(s, "$1???????")
+  }
+
+  private val renewalBarcodeRegex = "/renewals/[0-9]{15}".r.unanchored
+  private def maskRenewalBarcode(s: String): String = {
+    renewalBarcodeRegex.replaceAllIn(s, "/renewals/???????????????")
   }
 
   private def searchUrl(kibanaUrl: String) = s"$kibanaUrl/elasticsearch/*/_search"
